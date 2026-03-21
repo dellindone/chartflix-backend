5e038e3a75bfb23d17a3a35a46789a103c18372f
dellindone <adityav961@gmail.com>
2026-03-21 22:56:55 +0530

    chatgpt fixed dockerfile

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ app.include_router(user_router)
 
 import os
 import uvicorn
-
+print("PORT:", os.environ.get("PORT"))
 port = int(os.environ.get("PORT", 8000))
 
 uvicorn.run("main:app", host="0.0.0.0", port=port)
